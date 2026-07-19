90e7efb483eeae78eb32e6a52cf3f591e15ec631
Mr Admin Blue 𓃵 <vanguardmdbot@gmail.com>
2026-07-19 21:24:07 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Dach Xmd 🤖',
+  author: '‎',
+  botName: "DACH XMD LITE",
+  botOwner: 'Mr Admin Blue', // Your name
+  ownerNumber: '256745626308', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: '', // add your giphy key here 
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/mradminblue/dachxmdlite/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
